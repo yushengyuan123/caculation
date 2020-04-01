09d12d1ebbc374852272cc74d1c3120459cdf09e
Yu Shengyuan <1749238278@qq.com>
2020-04-01 13:12:38 +0800

    写了下生成表达式但是没有写完

--- core/expression.js
+++ core/expression.js
@@ -1,12 +1,62 @@
 const _isNumber = require('../share/utils').isNumber
+const getRandom = require('../share/utils').getRandom
+const print = require('../share/utils').printf
 
-const operator = ['+', '-', '*', '/']
+const operator = ['+', '-', '*', '÷']
+
 /**
- * 生成四则运算表达式
+ * 随机获得一个运算符
+ * @returns {string}
+ */
+function getOperator() {
+    return operator[Math.floor(Math.random() * (3 + 1))]
+}
+
+
+const _positive = function (expression) {
+    let exp = expression.split(' ')
+    if() {
+
+    } else {
+
+    }
+    print(exp)
+}
+
+_positive('9 - 5 + 1 =')
+
+/**
+ * 生成计算表达式
  * @param number
+ * @returns {['1 + 1 =', ...]}
  */
 const createExpression = function(number) {
     if(!_isNumber(number)) {
         throw new Error(`The ${number} is not a number type, please check again!`)
     }
+    let result = []
+    let operands = null
+    for (let i = 0; i < number; i++) {
+        operands = Math.floor(Math.random()* 3 + 1)
+        switch(operands) {
+            case 1: {
+                result.push(`${getRandom()} ${getOperator()} ${getRandom()} = `)
+                break
+            }
+            case 2: {
+                result.push(`${getRandom()} ${getOperator()} ${getRandom()} ${getOperator()} ${getRandom()} = `)
+                break
+            }
+            case 3: {
+                result.push(`${getRandom()} ${getOperator()} ${getRandom()} ${getOperator()} ${getRandom()} ${getOperator()} ${getRandom()} = `)
+                break
+            }
+            default: {
+                throw new Error('operands is not in the range of 3')
+            }
+        }
+    }
+    return result
 }
+
+
--- share/utils.js
+++ share/utils.js
@@ -4,8 +4,23 @@ const _isNumber = function(number) {
     return !(typeof number != "number" || number < 0);
 }
 
+//随机返回分数或者是整数
+//Math.floor(Math.random()*(m-n+1)+n)
+const getRandom = function() {
+    //随机决定是生成整数还是分数1表示整数，0表示分数
+    let isZ = Math.round(Math.random())
+    if(isZ) {
+        return Math.floor(Math.random() * 9 + 1)
+    } else {
+        let Denominator = Math.floor(Math.random() * 9 + 1)
+        let Molecule = Math.floor(Math.random() * 9 + 1)
+        return `${Denominator}/${Molecule}`
+    }
+}
+
 module.exports = {
     printf: print,
-    isNumber: _isNumber
+    isNumber: _isNumber,
+    getRandom: getRandom
 }
 
